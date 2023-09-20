16c5fdb146a7daf5f3ec07cda84b9553eed03545
Costelas Denis <81863134+denosg@users.noreply.github.com>
2023-09-20 18:09:31 +0300

    Feature/app theme (#1)
    * added logger
    
    * feature/adpp theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,38 @@
+import 'package:abac_challenge/common/custom_material_color.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // light theme
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      primary: Colors.white,
+      secondary: Colors.black,
+      tertiary: createMaterialColor(const Color.fromRGBO(167, 35, 58, 1)),
+    ),
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      },
+    ),
+    popupMenuTheme: const PopupMenuThemeData(color: Colors.white),
+  );
+  // dark theme
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+      primary: createMaterialColor(const Color.fromRGBO(1, 3, 7, 1)),
+      secondary: Colors.white70,
+      tertiary: createMaterialColor(const Color.fromRGBO(167, 35, 58, 1)),
+    ),
+    pageTransitionsTheme: const PageTransitionsTheme(
+      builders: {
+        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+        TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+      },
+    ),
+    popupMenuTheme: PopupMenuThemeData(
+        color: createMaterialColor(const Color.fromRGBO(10, 14, 18, 1))),
+  );
+}
