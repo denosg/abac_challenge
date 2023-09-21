edd47f70714e06b6156f7b8ae26611c5bdf5d2da
denos <costelasdenis@gmail.com>
2023-09-21 18:09:54 +0300

    first page changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:abac_challenge/pages/identify_car_page/identify_car_page.dart';
+import 'package:abac_challenge/pages/welcome_page/welcome_page.dart';
 import 'package:abac_challenge/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 void main() {
   //Makes it so SystemChrome.setPreferredOrientations works
@@ -22,6 +27,19 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
+      home: const WelcomePage(),
+      routes: {
+        WelcomePage.routeName: (context) => const WelcomePage(),
+        IdentifyCarPage.routeName: (context) => const IdentifyCarPage(),
+      },
     );
   }
 }
+
+/**
+ * COMMANDS:
+ * flutter pub run build_runner build --delete-conflicting-outputs
+ * dart format .
+ * flutter analyze
+ * flutter test
+ */
