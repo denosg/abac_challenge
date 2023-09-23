5e0186afe73e55b43719eb953203259f1f5218a4
Costelas Denis <81863134+denosg@users.noreply.github.com>
2023-09-23 15:32:18 +0300

    feature/changed_welcome_screen (#6)

--- lib/pages/welcome_page/welcome_page.dart
+++ lib/pages/welcome_page/welcome_page.dart
@@ -20,53 +20,51 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           children: [
             const Spacer(),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(32),
-                color: Colors.grey[100],
-              ),
-              child: Column(
-                children: [
-                  Center(
-                    child: Container(
-                      margin: const EdgeInsets.all(8),
-                      child: Text(
-                        'Bine ați venit !',
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: deviceSize.width * 0.1),
+                  margin: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Bine\nați\nvenit !',
                         style: TextStyle(
                             color: colors.secondary,
                             fontWeight: FontWeight.bold,
-                            fontSize: 28),
+                            fontSize: 50),
                       ),
+                      const SizedBox(width: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 120),
+                        child: Image.asset(
+                          'img/smiley_face.png',
+                          height: 80,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: colors.tertiary,
+                        padding: const EdgeInsets.all(10)),
+                    onPressed: () {
+                      logger.i('User is going to Identify Car Page');
+                      Navigator.of(context)
+                          .pushReplacementNamed(IdentifyCarPage.routeName);
+                    },
+                    child: const Text(
+                      'Să începem !',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                     ),
                   ),
-                  const SizedBox(height: 25),
-                  Center(
-                    child: Image.asset(
-                      'img/car_vector.png',
-                      width: deviceSize.width * 0.8,
-                    ),
-                  ),
-                  const SizedBox(height: 50),
-                  Center(
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: colors.tertiary,
-                          padding: const EdgeInsets.all(10)),
-                      onPressed: () {
-                        logger.i('User is going to Identify Car Page');
-                        Navigator.of(context)
-                            .pushReplacementNamed(IdentifyCarPage.routeName);
-                      },
-                      child: const Text(
-                        'Să începem !',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
             const Spacer(),
             const Spacer(),
