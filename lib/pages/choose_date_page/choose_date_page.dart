324632403583e037c9534e852a2b13099219cf83
Costelas Denis <81863134+denosg@users.noreply.github.com>
2023-09-24 17:54:37 +0300

    bug/fixed_render_issue_for_smaller_device (#11)

--- lib/pages/choose_date_page/choose_date_page.dart
+++ lib/pages/choose_date_page/choose_date_page.dart
@@ -1,5 +1,6 @@
 import 'package:abac_challenge/pages/choose_date_page/widgets/action_buttons_row.dart';
 import 'package:abac_challenge/pages/choose_date_page/widgets/date_time_picker.dart';
+import 'package:abac_challenge/pages/choose_date_page/widgets/hour_widget.dart';
 import 'package:abac_challenge/pages/identify_car_page/widgets/step_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,28 +15,25 @@ class ChooseDatePage extends ConsumerWidget {
     final ColorScheme colors = Theme.of(context).colorScheme;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: const ActionButtonsRow(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 30),
-              Text(
-                'LICITEAZĂ',
-                style: TextStyle(
-                    color: colors.tertiary,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24),
-              ),
-              const SizedBox(height: 30),
-              const StepIndicator(currentStep: 2),
-              const DateTimePicker(),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Text(
+              'LICITEAZĂ',
+              style: TextStyle(
+                  color: colors.tertiary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24),
+            ),
+            const SizedBox(height: 30),
+            const StepIndicator(currentStep: 2),
+            const DateTimePicker(),
+            const HourGridWidget(),
+            const ActionButtonsRow()
+          ],
         ),
       ),
     );
--- lib/pages/choose_date_page/widgets/date_time_picker.dart
+++ lib/pages/choose_date_page/widgets/date_time_picker.dart
@@ -1,5 +1,4 @@
 import 'package:abac_challenge/pages/choose_date_page/providers/date_picker_prov.dart';
-import 'package:abac_challenge/pages/choose_date_page/widgets/hour_widget.dart';
 import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,22 +11,17 @@ class DateTimePicker extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final pickedDate = useState<DateTime?>(null);
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        DatePicker(
-          DateTime.now(),
-          daysCount: 5,
-          initialSelectedDate: DateTime.now(),
-          selectionColor: Colors.black,
-          selectedTextColor: Colors.white,
-          onDateChange: (date) {
-            pickedDate.value = date;
-            ref.read(datePickerProv.notifier).setDay(date);
-          },
-        ),
-        const HourGridWidget(),
-      ],
+    return DatePicker(
+      DateTime.now(),
+      height: 81,
+      daysCount: 5,
+      initialSelectedDate: DateTime.now(),
+      selectionColor: Colors.black,
+      selectedTextColor: Colors.white,
+      onDateChange: (date) {
+        pickedDate.value = date;
+        ref.read(datePickerProv.notifier).setDay(date);
+      },
     );
   }
 }
